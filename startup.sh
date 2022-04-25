953988d42a7679430af2dd2478dae2b2dead3035
Debraj Kundu <kundudebraj4272@gmail.com>
2022-04-26 01:38:43 +0530

    debugging

--- startup.sh
+++ startup.sh
@@ -1,18 +1,21 @@
 #!/bin/bash
 echo "env vars..."
 echo "DB URL : " $DATABASE_URL
-export KC_DB_URL=jdbc:$DATABASE_URL
+# export KC_DB_URL=jdbc:$DATABASE_URL
+# echo "KC DB URL : " $KC_DB_URL
 regex='^postgres://([a-zA-Z0-9_-]+):([a-zA-Z0-9]+)@([a-z0-9.-]+):([[:digit:]]+)/([a-zA-Z0-9_-]+)$'
 if [[ $DATABASE_URL =~ $regex ]]; then
-    export DB_ADDR=${BASH_REMATCH[3]}
-    export DB_PORT=${BASH_REMATCH[4]}
-    export DB_DATABASE=${BASH_REMATCH[5]}
-    export DB_USER=${BASH_REMATCH[1]}
-    export DB_PASSWORD=${BASH_REMATCH[2]}
-
-    echo "DB_ADDR="$DB_ADDR", DB_PORT="$DB_PORT", DB_DATABASE="$DB_DATABASE", DB_USER="$DB_USER", DB_PASSWORD="$DB_PASSWORD
+    export KC_DB_URL_HOST=${BASH_REMATCH[3]}
+    export KC_DB_URL_PORT=${BASH_REMATCH[4]}
+    export KC_DB_URL_DATABASE=${BASH_REMATCH[5]}
+    export KC_DB_USERNAME=${BASH_REMATCH[1]}
+    export KC_DB_PASSWORD=${BASH_REMATCH[2]}
 fi
-echo "KC DB URL : " $KC_DB_URL
+echo "KC DB URL HOST : " $KC_DB_URL_HOST
+echo "KC DB URL PORT : " $KC_DB_URL_PORT
+echo "KC DB URL DATABASE : " $KC_DB_URL_DATABASE
+echo "KC DB USERNAME : " $KC_DB_USERNAME
+echo "KC DB PASSWORD : " $KC_DB_PASSWORD
 echo "Port : " $PORT
 echo "Starting up..."
-/opt/keycloak/bin/kc.sh start --db-url=$KC_DB_URL --http-port=$PORT
+/opt/keycloak/bin/kc.sh start --http-port=$PORT
