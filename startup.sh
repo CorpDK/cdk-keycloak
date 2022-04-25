5681e4fef665577b1828715e98abb9f83a225e34
Debraj Kundu <kundudebraj4272@gmail.com>
2022-04-25 22:55:00 +0530

    debugging

--- startup.sh
+++ startup.sh
@@ -5,4 +5,4 @@ export KC_DB_URL=$DATABASE_URL
 echo $KC_DB_URL
 echo $PORT
 echo "Starting up..."
-/opt/keycloak/bin/kc.sh start --http-port $PORT
+/opt/keycloak/bin/kc.sh start --db-url=$DATABASE_URL --http-port=$PORT
