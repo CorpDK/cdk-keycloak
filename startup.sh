e5c73f83a62d359f54b44b80bac20d339b65a797
Debraj Kundu <kundudebraj4272@gmail.com>
2022-04-25 22:44:46 +0530

    debugging

--- startup.sh
+++ startup.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 echo "env vars..."
 echo $DATABASE_URL
+export KC_DB_URL=$DATABASE_URL
+echo $KC_DB_URL
 echo $PORT
 echo "Starting up..."
-/opt/keycloak/bin/kc.sh start --db-url $DATABASE_URL --http-port $PORT
+/opt/keycloak/bin/kc.sh start --http-port $PORT
